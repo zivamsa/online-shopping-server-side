e566625e643e1ac911e8d44c89e79274900609c3
Dolev <www.dolev@gmail.com>
2023-08-12 14:51:47 +0300

    added cors support

--- src/main/java/com/example/FinalProject1/FinalProject1Application.java
+++ src/main/java/com/example/FinalProject1/FinalProject1Application.java
@@ -3,8 +3,11 @@ package com.example.FinalProject1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@ComponentScan({"com.example.FinalProject1.controllers","com.example.FinalProject1.services"})
 //@EntityScan("com.example.FinalProject1.models")
@@ -14,6 +17,16 @@ public class FinalProject1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalProject1Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
