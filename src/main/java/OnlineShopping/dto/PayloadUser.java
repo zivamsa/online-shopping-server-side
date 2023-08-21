8715cb74bd6e5f35a6dd139ca174981ab9655c73
Dolev <www.dolev@gmail.com>
2023-08-21 23:49:02 +0300

    user payload custom validation

--- src/main/java/OnlineShopping/dto/PayloadUser.java
+++ src/main/java/OnlineShopping/dto/PayloadUser.java
@@ -1,6 +1,7 @@
 package OnlineShopping.dto;
 
 import OnlineShopping.models.Role;
+import OnlineShopping.validation.NullableNotBlank;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.Data;
@@ -9,12 +10,17 @@ import lombok.Data;
 public class PayloadUser {
     private Integer id;
 
+    @NullableNotBlank
     @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     private String email;
+    @NullableNotBlank
     private String address;
+    @NullableNotBlank
     private String firstName;
+    @NullableNotBlank
     private String lastName;
 
+    @NullableNotBlank
     @Enumerated(EnumType.STRING)
     private Role role;
 }
---
+++ src/main/java/OnlineShopping/validation/NullableNotBlank.java
@@ -0,0 +1,18 @@
+package OnlineShopping.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = NullableNotBlankValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface NullableNotBlank {
+    String message() default "Can't be empty";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+
+}
