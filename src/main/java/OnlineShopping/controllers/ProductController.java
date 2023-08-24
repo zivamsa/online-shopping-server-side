673a315755afc7894355a2312711a09ffc3a7c35
Dolev <www.dolev@gmail.com>
2023-08-24 18:11:55 +0300

    upload image on update as well

--- src/main/java/OnlineShopping/controllers/ProductController.java
+++ src/main/java/OnlineShopping/controllers/ProductController.java
@@ -1,10 +1,7 @@
 package OnlineShopping.controllers;
 
 import OnlineShopping.models.Product;
-import OnlineShopping.exceptions.LackingPermissions;
-import OnlineShopping.services.FileStorageService;
 import OnlineShopping.services.ProductService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -19,9 +16,6 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
-    @Autowired
-    FileStorageService fileStorageService;
-
     @GetMapping("/products")
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
@@ -32,28 +26,22 @@ public class ProductController {
         return productService.getProductById(id);
     }
 
-    @PostMapping(path = "/product", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+    @PostMapping(path = "/product", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public Product addProduct(
-            @RequestPart(name="product") @Valid Product product,
-            @RequestPart(name="image", required = false) MultipartFile image
-    ){
+            @RequestPart(name = "product") @Valid Product product,
+            @RequestPart(name = "image", required = false) MultipartFile image
+    ) {
         //      INPUT FORMAT: form-data
         //      image: image
         //      product: Product in json format (as string)- use Content-Type: application/json on this field
-        final Product savedProduct = productService.saveOrUpdate(product);
-
-        // TODO: maybe- store images as an entity, connect to the product
-        // TODO: upload file in update as well
-        // TODO: return the image to the client(@Transient maybe)
-        if (image != null) {
-            fileStorageService.uploadProduct(image, savedProduct.getId());
-        }
-
-        return savedProduct;
+        return productService.saveOrUpdate(product, image);
     }
 
     @PutMapping("/product")
-    public Product updateProduct(@Valid @RequestBody Product product) {
-        return productService.saveOrUpdate(product);
+    public Product updateProduct(
+            @RequestPart(name = "product") @Valid Product product,
+            @RequestPart(name = "image", required = false) MultipartFile image
+    ) {
+        return productService.saveOrUpdate(product, image);
     }
 }
--- src/main/java/OnlineShopping/services/ProductService.java
+++ src/main/java/OnlineShopping/services/ProductService.java
@@ -4,6 +4,7 @@ import OnlineShopping.models.Product;
 import OnlineShopping.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +15,10 @@ public class ProductService {
     @Autowired
     ProductRepository repository;
 
+    @Autowired
+    FileStorageService fileStorageService;
+
+
     public Product getProductById(Long id) {
         return repository.findById(id).get();
     }
@@ -23,7 +28,19 @@ public class ProductService {
         repository.findAll().forEach(product -> products.add(product));
         return products;
     }
+    public Product saveOrUpdate(Product product, MultipartFile image) {
+        // TODO: maybe- store images as an entity, connect to the product
+        // TODO: return the image to the client(@Transient maybe)
+
+        Product savedProduct = repository.save(product);
+        if (image != null) {
+            fileStorageService.uploadProduct(image, savedProduct.getId());
+        }
+
+        return savedProduct;
+    }
+
     public Product saveOrUpdate(Product product) {
-        return repository.save(product);
+        return saveOrUpdate(product, null);
     }
 }
